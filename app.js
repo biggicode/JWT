1d11d490e9a25f1974893a2727041892aed80b91
Oncescu Laurentiu <larioncescu@yahoo.com>
2022-11-26 13:54:13 +0200

    add main router

--- app.js
+++ app.js
@@ -4,12 +4,15 @@ require("express-async-errors");
 const express = require("express");
 const app = express();
 
+const mainRouter = require("./routes/main");
 const errorHandlerMiddleware = require("./middleware/error-handle");
 const notFoundMiddlerware = require("./middleware/not-found");
 
 //middleware
 app.use(express.static("./public"));
 app.use(express.json());
+
+app.use("/api/v1", mainRouter);
 
 app.use(notFoundMiddlerware);
 app.use(errorHandlerMiddleware);
