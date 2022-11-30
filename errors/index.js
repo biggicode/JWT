5b4d8ca3d037af79ac3c2abae32085216f366c17
Oncescu Laurentiu <larioncescu@yahoo.com>
2022-11-30 15:29:45 +0200

    more error classes

---
+++ errors/index.js
@@ -0,0 +1,9 @@
+const CustomAPIError = require("./custome-error");
+const BadRequestError = require("./bad-request");
+const UnauthenticatedError = require("./unauthenticated");
+
+module.exports = {
+  CustomAPIError,
+  BadRequestError,
+  UnauthenticatedError,
+};
